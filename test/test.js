ae881a9ad540954db96615ef111f3574f710131a
Samuel Maisonneuve <maisonneuve@codelutin.com>
2015-05-21 11:45:29 +0200

    #2 - Add test on transform

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@
 
 var assert = require("assert");
 var concat = require("../index.js");
+var fs = require("fs");
 
 describe("babel-concat", function() {
 
@@ -11,6 +12,26 @@ describe("babel-concat", function() {
             "test/files/File1.js",
             "test/files/File1.js",
             "test/files/File1.js"
+        ], {
+            sourceMap: true
+        });
+
+        it("Should return a result with code and map", function() {
+            assert.equal(true, !!result);
+            assert.equal(true, !!result.code);
+            assert.equal(true, !!result.map);
+        });
+    });
+
+    describe("#transform", function() {
+        var file1 = fs.readFileSync("test/files/File1.js", "utf-8"),
+            file2 = fs.readFileSync("test/files/File2.js", "utf-8"),
+            file3 = fs.readFileSync("test/files/File3.js", "utf-8");
+
+        var result = concat.transform([
+            file1,
+            file2,
+            file3
         ], {
             sourceMap: true
         });
